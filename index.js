cd2f1150f2bd6cac097e5728a129bebfa19b6bb7
Aditya Vaidya <sadityasss@hotmail.com>
2022-01-11 00:53:05 +0530

    cors and socket io added

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const http = require('http')
+const socketIO = require('socket.io')
+const cors = require('cors')
 const env = require('dotenv')
 env.config({ path : "./config/server.env"})
 const cookieParser = require('cookie-parser')
@@ -11,7 +14,27 @@ const connectDB = require('./config/db')
 
 const app = express();
 connectDB()
+const server = http.createServer(app)
+const io = socketIO(server, {
+  transports:['polling'],
+  cors:{
+    cors: {
+      origin: "http://localhost:3000"
+    }
+  }
+})
 
+io.on('connection', (socket) => {
+  console.log('A user is connected');
+
+  socket.on('message', (message) => {
+    console.log(`message from ${socket.id} : ${message}`);
+  })
+
+  socket.on('disconnect', () => {
+    console.log(`socket ${socket.id} disconnected`);
+  })
+})
 
 app.use(cookieParser())
 app.use(express.json())
@@ -22,6 +45,6 @@ app.use('/api/auth',authRoute)
 app.use('/api',userRoute)
 app.use('/api/room',roomRoute)
 
-app.listen(process.env.PORT,()=>{
+server.listen(process.env.PORT,()=>{
     console.log(`listening on port ${process.env.PORT}`)
 })
